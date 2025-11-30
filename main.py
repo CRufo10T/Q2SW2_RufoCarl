79268f175e2027bd9298d709bd0dc4530effbf86
CRufo10T <rufo.cg@obmontessori.edu.ph>
2025-11-30 23:09:20 +0800

    Refactor scoring function and improve calculations
    Updated function names and fixed subject indexing for score retrieval. Added calculations for all subjects and their averages.

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 from pyscript import display, document
 
-subjects = ['Science','SS','English','Filipino', 'TLE', 'VE'] # using Tuples for the subjects
+subjects = ['science','ss','english','filipino', 'tle', 've'] # using Tuples for the subjects
 units = (5,3,5,3,1,1) # using list for the units
 
-def funct(e):
+def give(e):
     first_name = document.getElementById("first-name").value
     last_name = document.getElementById("last-name").value
     
     science_score = int(document.getElementById (subjects[0]).value)
     ss_score = int(document.getElementById (subjects[1]).value)
-    english_score = int(document.getElementById (subjects[0]).value)
-    filipino_score = int(document.getElementById (subjects[0]).value)
-    TLE_score = int(document.getElementById (subjects[0]).value)
-    VE_score = int(document.getElementById (subjects[0]).value)
-    
+    english_score = int(document.getElementById (subjects[2]).value)
+    filipino_score = int(document.getElementById (subjects[3]).value)
+    TLE_score = int(document.getElementById (subjects[4]).value)
+    VE_score = int(document.getElementById (subjects[5]).value)
+
     science_average = science_score * units[0]
     ss_average = ss_score * units[1]
     english_average = english_score * units[2]
-    
+    filipino_average = filipino_score * units[3]
+    TLE_average = TLE_score * units[4]
+    VE_average = VE_score * units[5]
 
-    display(subjects(0), output='SubjectA')
-    display(subjects(1), output='SubjectB')
-    display(subjects(2), output='SubjectC')
-    display(subjects(3), output='SubjectD')
-    display(subjects(4), output='SubjectE')
-    display(subjects(5), output='SubjectF')
-    display(subjects(6), output='SubjectG')
+    total_units = units[0] + units[1] + units[2] + units[3] + units[4] + units[5]
+    average = (science_average + ss_average + english_average + filipino_average + TLE_average + VE_average) / total_units
+
+    document.getElementById("output").innerHTML = ''
+
+    display(f"Name: {first_name} {last_name}", target='output')
+    display("Average:", target="output")
+    display(f"{subjects[0]}: {science_score}", target="output")
+    display(f"{subjects[1]}: {ss_score}", target="output")
+    display(f"{subjects[2]}: {english_score}", target="output")
+    display(f"{subjects[3]}: {filipino_score}", target="output")
+    display(f"{subjects[4]}: {TLE_score}", target="output")
+    display(f"{subjects[5]}: {VE_score}", target="output")
+
+    display(f"General Weighted Average: {average}", target="output")
+
+
 
