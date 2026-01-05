12b196c039bed5552d385de2c9918ed582eefcfe
CRufo10T <rufo.cg@obmontessori.edu.ph>
2026-01-05 09:15:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from pyscript import display, document
 
-subjects = ['science','ss','english','filipino', 'tle', 've'] # using Tuples for the subjects
-units = (5,3,5,3,1,1) # using list for the units
+subjects = ['science','ss','english','filipino', 'tle', 've']
+units = (5,3,5,3,1,1)
 
 def give(e):
     first_name = document.getElementById("first-name").value
     last_name = document.getElementById("last-name").value
     
-    science_score = int(document.getElementById (subjects[0]).value)
-    ss_score = int(document.getElementById (subjects[1]).value)
-    english_score = int(document.getElementById (subjects[2]).value)
-    filipino_score = int(document.getElementById (subjects[3]).value)
-    TLE_score = int(document.getElementById (subjects[4]).value)
-    VE_score = int(document.getElementById (subjects[5]).value)
+    science_score = int(document.getElementById(subjects[0]).value)
+    ss_score = int(document.getElementById(subjects[1]).value)
+    english_score = int(document.getElementById(subjects[2]).value)
+    filipino_score = int(document.getElementById(subjects[3]).value)
+    TLE_score = int(document.getElementById(subjects[4]).value)
+    VE_score = int(document.getElementById(subjects[5]).value)
 
     science_average = science_score * units[0]
     ss_average = ss_score * units[1]
@@ -21,25 +21,22 @@ def give(e):
     TLE_average = TLE_score * units[4]
     VE_average = VE_score * units[5]
 
-    total_units = units[0] + units[1] + units[2] + units[3] + units[4] + units[5]
-    average = (science_average + ss_average + english_average + filipino_average + TLE_average + VE_average) / total_units
+    total_units = sum(units)
+    average = (
+        science_average + ss_average + english_average +
+        filipino_average + TLE_average + VE_average
+    ) / total_units
 
-    document.getElementById("output").innerHTML = ''
+    document.getElementById("output").innerHTML = ""
 
-    display(f"Name: {first_name} {last_name}", target='output')
-    display("Average:", target="output")
-    display(f"{subjects[0]}: {science_score}", target="output")
-    display(f"{subjects[1]}: {ss_score}", target="output")
-    display(f"{subjects[2]}: {english_score}", target="output")
-    display(f"{subjects[3]}: {filipino_score}", target="output")
-    display(f"{subjects[4]}: {TLE_score}", target="output")
-    display(f"{subjects[5]}: {VE_score}", target="output")
-
+    display(f"Name: {first_name} {last_name}", target="output")
     display(f"General Weighted Average: {average}", target="output")
-        if average > 75:
+
+    if average > 75:
         display("You passed", target="output")
     else:
         display("You failed", target="output")
+
 
 
 
